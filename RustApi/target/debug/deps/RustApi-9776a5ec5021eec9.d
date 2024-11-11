31503b422158da9d467a507221a10e92bed50f7f
SaffronGamer <saffrongamer167@gmail.com>
2024-11-11 11:16:11 -0700

    Basic setup and trying to set up custom icons for the Tauri Project

---
+++ RustApi/target/debug/deps/RustApi-9776a5ec5021eec9.d
@@ -0,0 +1,5 @@
+C:\Users\chase\Documents\rust_projects\Cadre Creator\RustApi\target\debug\deps\libRustApi-9776a5ec5021eec9.rmeta: src/main.rs
+
+C:\Users\chase\Documents\rust_projects\Cadre Creator\RustApi\target\debug\deps\RustApi-9776a5ec5021eec9.d: src/main.rs
+
+src/main.rs:
